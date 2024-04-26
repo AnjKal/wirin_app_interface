42ba650ddd71010c93407eb88d3f5b3ae4f94780
AnjKal <162329260+AnjKal@users.noreply.github.com>
2024-04-26 12:30:57 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+import webbrowser
+
+# URL for Google Play Store
+play_store_url = "https://play.google.com/store"
+
+# Open the Google Play Store in the default browser
+webbrowser.open(play_store_url)
